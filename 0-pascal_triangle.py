4e17c03840ef965c1b8e03203b6ea49b97779541
Brainstorma ⚔️ <113864294+Brainstorma@users.noreply.github.com>
2023-07-27 01:00:02 +0000

    Update 0-pascal_triangle.py

--- 0-pascal_triangle.py
+++ 0-pascal_triangle.py
@@ -1,21 +1,27 @@
 #!/usr/bin/python3
-'''A module for working with Pascal's triangle.
-'''
+"""Pascal Triangle Interview Challenge for Alx project conducted by Dr Marcus"""
 
 
 def pascal_triangle(n):
-    '''Creates a list of lists of integers representing
-    the Pascal's triangle of a given integer.
-    '''
-    triangle = []
-    if type(n) is not int or n <= 0:
-        return triangle
+    """returns a list of lists of numbers
+    representing the pascal triangle"""
+    if n <= 0:
+        return []
+
+    pascal_triangle = [0] * n
+
     for i in range(n):
-        line = []
-        for j in range(i + 1):
-            if j == 0 or j == i:
-                line.append(1)
-            elif i > 0 and j > 0:
-                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
-        triangle.append(line)
-    return triangle
+        # This code snipet define and explained that  a row is  fill first and last idx with 1
+        row = [0] * (i+1)
+        row[0] = 1
+        row[len(row) - 1] = 1
+
+        for j in range(1, i):
+            if j > 0 and j < len(row):
+                x = pascal_triangle[i - 1][j]
+                y = pascal_triangle[i - 1][j - 1]
+                row[j] = x + y
+
+        pascal_triangle[i] = row
+
+    return pascal_triangle
